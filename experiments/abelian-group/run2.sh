67c011f074ab4affd810831447081eba236a13e1
Raúl López <sultanofwolves@gmail.com>
2022-01-03 11:39:24 +0100

    Experimental files

---
+++ experiments/abelian-group/run2.sh
@@ -0,0 +1,5 @@
+timeout 21600 maude ../../native-narrowing.maude ../../testing-modules/abelian-group.maude ./row10.maude  > ./outputs/abelian-group-row10.txt & disown
+sleep 2
+timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/abelian-group.maude ./row11.maude  > ./outputs/abelian-group-row11.txt & disown
+sleep 2
+timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/abelian-group.maude ./row12.maude  > ./outputs/abelian-group-row12.txt & disown
---
+++ experiments/idempotence-vending-machine/run1.sh
@@ -0,0 +1,17 @@
+timeout 21600 maude ../../native-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row1.maude  > ./outputs/idempotence-vending-machine-row1.txt & disown
+sleep 2
+timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row2.maude  > ./outputs/idempotence-vending-machine-row2.txt & disown
+sleep 2
+timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row3.maude  > ./outputs/idempotence-vending-machine-row3.txt & disown
+sleep 2
+timeout 21600 maude ../../native-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row4.maude  > ./outputs/idempotence-vending-machine-row4.txt & disown
+sleep 2
+timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row5.maude  > ./outputs/idempotence-vending-machine-row5.txt & disown
+sleep 2
+timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row6.maude  > ./outputs/idempotence-vending-machine-row6.txt & disown
+sleep 2
+timeout 21600 maude ../../native-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row7.maude  > ./outputs/idempotence-vending-machine-row7.txt & disown
+sleep 2
+timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row8.maude  > ./outputs/idempotence-vending-machine-row8.txt & disown
+sleep 2
+timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/idempotence-vending-machine.maude ./row9.maude  > ./outputs/idempotence-vending-machine-row9.txt & disown

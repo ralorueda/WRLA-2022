6d2a28e1bc0ed1b16ecdeb6b7408621cb910b98d
Raúl López <sultanofwolves@gmail.com>
2022-02-23 10:06:43 +0100

    test executions fixed

--- experiments/xor-protocol/run.sh
+++ experiments/xor-protocol/run.sh
@@ -1,5 +1,5 @@
 timeout 21600 maude ../../native-narrowing.maude ../../testing-modules/xor-protocol.maude ./row1.maude  > ./outputs/xor-protocol-row1.txt & disown
 sleep 2
-timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/xor-protocol.maude ./row2.maude  > ./outputs/xor-protocol-row2.txt & disown
+timeout 21600 maude ../../canonical-narrowing.maude ../../testing-modules/xor-protocol.maude ./row2.maude  > ./outputs/xor-protocol-row2.txt & disown
 sleep 2
-timeout 21600 maude ../../mgu-narrowing.maude ../../testing-modules/xor-protocol.maude ./row3.maude  > ./outputs/xor-protocol-row3.txt & disown
+timeout 21600 maude ../../canonical-narrowing.maude ../../testing-modules/xor-protocol.maude ./row3.maude  > ./outputs/xor-protocol-row3.txt & disown
